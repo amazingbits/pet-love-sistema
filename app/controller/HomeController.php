d22967a06ce4789ccba4308eda44ea44e719b45c
Guilherme Andrade <gui-keanne@hotmail.com>
2021-11-01 13:38:31 -0300

    implementando lista de agendas para os proximos dias

--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Core\DefaultController;
 use App\Helper\CurrentInformationHelper;
+use App\Helper\JWTHelper;
 
 class HomeController extends DefaultController
 {
@@ -13,8 +14,31 @@ class HomeController extends DefaultController
             header("Location: " . BASE_PATH . "/auth");
         }
         $currentInformationHelper = new CurrentInformationHelper();
+
+        $jwtHelper = new JWTHelper();
+        $userInfo = $jwtHelper->decryptJWT($_COOKIE["hash"]);
+        $userId = (int)$userInfo->id;
+
+        $ctx = stream_context_create([
+            "http" => [
+                "method" => "GET",
+                "header" => [
+                    "Content-type: application/json\r\n"
+                ],
+                "ignore_errors" => true
+            ]
+        ]);
+
+        $agendas = file_get_contents(API_URL . "/agendaitem/agendaproximosdias/" . $userId, null, $ctx);
+        if(http_response_code() !== 200) {
+            $agendas = [];
+        } else {
+            $agendas = json_decode($agendas);
+        }
+
         $data = [
-            "now" => $currentInformationHelper->getCurrentDateInformation()
+            "now" => $currentInformationHelper->getCurrentDateInformation(),
+            "agendas" => $agendas
         ];
 
         $this->render("home", $data);
--- app/view/home.twig.php
+++ app/view/home.twig.php
@@ -13,79 +13,39 @@
 
 <div class="title">
     <div></div>
-    <h3>Hoje</h3>
+    <h3>Próximos Dias</h3>
     <div></div>
 </div>
 
-<div class="legend">
-    <div class="legend-single">
-        <div style="background-color: #fff;"></div>
-        <span>Marcada</span>
-    </div>
-    <div class="legend-single">
-        <div style="background-color: #66BB6A;"></div>
-        <span>Confirmada</span>
-    </div>
-    <div class="legend-single">
-        <div style="background-color: #EF5350;"></div>
-        <span>Cancelada</span>
-    </div>
-</div>
+<div class="my_agendas">
+    <table>
+        <thead>
+            <tr>
+                <th>Agenda</th>
+                <th>Descrição</th>
+                <th>Status</th>
+                <th>Dia</th>
+                <th>Hora</th>
+            </tr>
+        </thead>
+        <tbody>
+            {% for agenda in agendas %}
+            <tr>
+                <td>{{agenda.descAgenda}} - {{agenda.diaSemana}}</td>
+                <td>{{agenda.descricao}}</td>
+                <td>{{agenda.status}}</td>
+                <td>{{agenda.data}}</td>
+                <td>{{agenda.hora}}</td>
+            </tr>
+            {% endfor %}
 
-<div class="schedule-card sc-cancelled">
-    <div class="sc-info">
-        <img src="{{BASE_PATH}}/assets/img/dog-default.png" alt="Dog">
-        <p>Rex <i class="fas fa-mars"></i></p>
-        <p>Banho e Tosa</p>
-        <p>10:00</p>
-    </div>
-    <div class="sc-options">
-        <a href="#" class="abrirExame" title="Abrir"><i class="fas fa-eye"></i></a>
-        <a href="#" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
-        <a href="#" title="Telefone"><i class="fas fa-phone"></i></a>
-    </div>
-</div>
-
-<div class="schedule-card sc-marked">
-    <div class="sc-info">
-        <img src="{{BASE_PATH}}/assets/img/dog-default.png" alt="Dog">
-        <p>Lilica <i class="fas fa-venus"></i></p>
-        <p>Banho e Tosa</p>
-        <p>10:30</p>
-    </div>
-    <div class="sc-options">
-        <a href="#" class="abrirExame" title="Abrir"><i class="fas fa-eye"></i></a>
-        <a href="#" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
-        <a href="#" title="Telefone"><i class="fas fa-phone"></i></a>
-    </div>
-</div>
-
-<div class="schedule-card sc-marked">
-    <div class="sc-info">
-        <img src="{{BASE_PATH}}/assets/img/dog-default.png" alt="Dog">
-        <p>Romeu <i class="fas fa-mars"></i></p>
-        <p>Banho e Tosa</p>
-        <p>11:00</p>
-    </div>
-    <div class="sc-options">
-        <a href="#" class="abrirExame" title="Abrir"><i class="fas fa-eye"></i></a>
-        <a href="#" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
-        <a href="#" title="Telefone"><i class="fas fa-phone"></i></a>
-    </div>
-</div>
-
-<div class="schedule-card">
-    <div class="sc-info">
-        <img src="{{BASE_PATH}}/assets/img/dog-default.png" alt="Dog">
-        <p>Toby <i class="fas fa-mars"></i></p>
-        <p>Banho e Tosa</p>
-        <p>11:30</p>
-    </div>
-    <div class="sc-options">
-        <a href="#" class="abrirExame" title="Abrir"><i class="fas fa-eye"></i></a>
-        <a href="#" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
-        <a href="#" title="Telefone"><i class="fas fa-phone"></i></a>
-    </div>
+            {% if agendas == null %}
+            <tr>
+                <td colspan="5">Nenhuma atividade encontrada...</td>
+            </tr>
+            {% endif %}
+        </tbody>
+    </table>
 </div>
 
 {% endblock %}
